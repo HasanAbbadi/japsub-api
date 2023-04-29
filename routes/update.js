e593b73b166bbc261b6d89adb1a01a83aba8e18d
HasanAbbadi <hasanabady55@gmail.com>
2023-04-29 20:45:40 +0300

    fixed not updating bug

--- routes/update.js
+++ routes/update.js
@@ -7,37 +7,50 @@ const Update = async () => {
 
   const lastUpdate = new Date(data.last_updated);
 
+  const count = 0;
   for (let i = 0; i < newData.length; i++) {
     const currUpdate = new Date(newData[i].date);
     if (currUpdate <= lastUpdate) return { msg: "Up to Date." };
 
-    if (newData[i].title == data.data[i].title) {
-      await updateEntry(i, newData[i]);
+    count += 1;
+    const { exists, index } = checkEntry(newData[i].title);
+    if (exists) {
+      await updateEntry(index, newData[i]);
     } else {
       await addEntry(newData[i]);
     }
 
-    return { msg: "Updated" };
+    return { msg: "Updated.", count };
   }
+};
+
+const checkEntry = (title) => {
+  for (let i = 0; i < data.length; i++) {
+    if (data.data[i].title == title) {
+      return { exists: true, index: i };
+    }
+  }
+  return { exists: false, index: 0 };
 };
 
 const updateEntry = async (index, entry) => {
   data.data[index] = entry;
   data.data[index].subs = await getTitleSub(entry.url);
 
-  updateFile();
+  updateFile(data.data[index].date);
 };
+
 const addEntry = async (entry) => {
+  entry.subs = await getTitleSub(entry.url);
   data.data.push(entry);
 
-  updateFile();
+  updateFile(entry.date);
 };
 
-const updateFile = () => {
+const updateFile = (date) => {
   // update to last file date instead of using new Date()
   // to avoid conflicting timezones and local clocks.
-
-  data.last_updated = data.data[0].date
+  data.last_updated = date;
 
   const json = JSON.stringify(data, null, 2);
   fs.writeFile("data/all.json", json, (err) => {
